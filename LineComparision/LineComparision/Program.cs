207ada3890795f410eab76b9ff95af7a5b5d0ed5
guri97 <guri84096@gmail.com>
2022-06-09 12:33:28 +0530

    [gurpreet] UC1_LineLength

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LineComparision
+
+{
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+         
+            Console.WriteLine("Welcome to the line comparision problem solution");
+            UC1_LineLength Line = new UC1_LineLength();
+
+            Line.Distancecalc();
+
+
+
+        }
+    }
+}
+
---
+++ LineComparision/LineComparision/UC1_LineLength.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    internal class UC1_LineLength
+    {
+      
+        
+             public void Distancecalc()
+            {
+
+                Console.WriteLine("Enter the value for x1");
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value for y1");
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value for x2");
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value for y2");
+                int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+                double var1 = Math.Pow((x2 - x1), 2);
+                double var2 = Math.Pow((y2 - y1), 2);
+                double Result = Math.Sqrt(var1 + var2);
+                Console.WriteLine("distance between coordinates {0},{1} and {2},{3} is {4}", x1, y1, x2, y2, Result);
+
+
+
+
+
+
+            }
+        }
+    }
+
